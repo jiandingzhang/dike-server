34a5691bf03559b2cec89d9c2e9206753c3efd6b
squidward <13617825707@163.com>
2025-06-04 11:53:06 +0800

    init: 迪客后端项目初始化

---
+++ dike-server/src/main/java/com/itom/DikeApplication.java
@@ -0,0 +1,11 @@
+package com.itom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DikeApplication.class, args);
+    }
+}
---
+++ dike-server/src/test/java/com/itom/DikeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DikeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
