356a2d46f872c6a48fcbc9f16eb221fcd9350aa0
squidward <13617825707@163.com>
2025-06-06 17:00:53 +0800

    fix: 依赖版本集中控制，结构目录调整

--- dike-server/src/main/java/com/itom/DikeApplication.java
+++ dike-server/src/main/java/com/itom/DikeApplication.java
@@ -1,9 +1,11 @@
 package com.itom;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.itom.mapper")
 public class DikeApplication {
     public static void main(String[] args) {
         SpringApplication.run(DikeApplication.class, args);
